7089ca6c14bdbfbb90b13473ce4274f0acf90f27
Alexander Nguyen <cernvii@gmail.com>
2021-01-27 17:43:03 -0500

    complete the char counter on tweet and basic tweet container html

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  document.querySelector(".tweet-input").addEventListener('keyup', (event) => {
+    let count = $(".tweet-counter").find(".counter");
+    let textCount = $(this).find("#tweet-text").val().length;
+    countUpdate = $(count.val((140 - textCount)));
+
+    if (countUpdate.val() < 0) {
+      $(count).css({color: 'red'});
+    } else {
+      $(count).css({color: 'black'});
+    }
+  })
+});
