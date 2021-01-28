e7afd7b3d20fe500e5ef35a59697d56bd8adbb79
Alexander Nguyen <cernvii@gmail.com>
2021-01-28 03:01:30 -0500

    trying to get prepend to work

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,10 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const renderTweets = (tweets) => {
-  $('.tweet-container').prepend(createTweetElement(tweets));
-}
-
 const data = [
   {
     "user": {
@@ -33,9 +29,16 @@ const data = [
   }
 ]
 
+
+const renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    $('.tweet-container').prepend(createTweetElement(tweet));
+  }
+}
+
 const createTweetElement = (tweetData) => {
   let content = `<article class="tweet"> 
-   <header class="tweeted-header">
+   <header class="tweet-header">
     <div class= "tweeted-profile">
       <img class="icon" src=${tweetData.user.avatars} width = "50", height = "50">
       <span>${tweetData.user.name}</span>
@@ -46,7 +49,7 @@ const createTweetElement = (tweetData) => {
         ${tweetData.content.text}
       </p>
     <footer>
-      <small>${tweetData.user.created_at}
+      <small>${tweetData.created_at}
       <button class = "nav-text-right"> 
       buttons
     </button>
@@ -55,19 +58,5 @@ const createTweetElement = (tweetData) => {
     
   return content
 }
-
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
-
-const $tweet = createTweetElement(tweetData);
 
 renderTweets(data);
